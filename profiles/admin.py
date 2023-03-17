e5ba6698ec58b2a4628cb3b8ca98076fb13eb672
JackDay94 <Jack94day@gmail.com>
2023-03-17 20:06:41 +0000

    feat: Add Profile model to admin and fix signals to work on user creation

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,16 @@
+"""Admin for the Profiles App"""
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Class which handles what the admin can see from
+    the admin dashboard of the Profile model and what
+    can be filtered and searched.
+    """
+    list_display = (
+        'user',
+        )
+    search_fields = ['user__username',]
