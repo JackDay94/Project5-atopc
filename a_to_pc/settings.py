cfec5eb96dd5061cd1985f0caeb4a93fec9c4121
JackDay94 <Jack94day@gmail.com>
2023-03-21 20:28:47 +0000

    feat: Add Category and Product models to products app and update admin.py for products app

--- a_to_pc/settings.py
+++ a_to_pc/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'profiles',
+    'products',
 
     # other
     'crispy_forms',
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'average_rating',
+        'image',
+    )
+
+    list_filter = (
+        'category',
+    )
+
+    search_fields = (
+        'name',
+        'sku',
+        'category',
+    )
+
+    ordering = ('sku',)
