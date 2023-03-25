b3b172f8b84ec3241a8e1d555ad4bea609fe5e5e
JackDay94 <Jack94day@gmail.com>
2023-03-25 18:55:10 +0000

    feat: Add template, view and url for deleting products from the store

--- products/views.py
+++ products/views.py
@@ -175,3 +175,29 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """
+    Delete a product from the store.
+    Modified from Boutique Ado walkthrough.
+    """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        product.delete()
+        messages.success(request, 'Successfully deleted product!')
+        return redirect(reverse('home'))
+
+    template = 'products/delete_product.html'
+    context = {
+        'product': product
+    }
+
+    return render(request, template, context)
