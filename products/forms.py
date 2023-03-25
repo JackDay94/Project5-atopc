5142f1e0a39d80600a53e06daea337b3805de75c
JackDay94 <Jack94day@gmail.com>
2023-03-25 13:46:34 +0000

    feat: Add boilerplate for add product.html and product form from Boutique Ado walkthrough

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,7 @@
-from .models import Review
+from .models import Review, Product
 from django.forms import ModelForm
+from django import forms
+from .widgets import CustomClearableFileInput
 
 
 class ReviewForm(ModelForm):
@@ -12,3 +14,26 @@ class ReviewForm(ModelForm):
             "rating",
             "review_content",
         )
+
+
+class ProductForm(ModelForm):
+    """
+    Displays the product form.
+    From CI Boutique Ado walkthrough.
+    """
+    class Meta:
+        model = Product
+        fields = '__all__'
+        exclude = ('average_rating',)
+
+    image = forms.ImageField(label='Image', required=False,
+                             widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black'
