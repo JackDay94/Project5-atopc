6df56e1fc03bf1be4d536bc5d5cfaae86f947a48
JackDay94 <Jack94day@gmail.com>
2023-03-26 16:39:44 +0000

    feat: Add add_to_bag views and urls to add products to bag and update add to bag links

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_bag(request):
@@ -8,3 +11,27 @@ def view_bag(request):
     """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """
+    A view to add a quantity of the specified product to the shopping bag.
+    From CI Boutique Ado walkthrough.
+    """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}'
+            )
+    else:
+        bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
