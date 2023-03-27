c1832b4e5d0a6f059842968509220c59913e8e5c
JackDay94 <Jack94day@gmail.com>
2023-03-27 23:55:46 +0000

    feat: Add checks in webhook handler to find existing orders in the database

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,10 @@ import json
 
 @require_POST
 def cache_checkout_data(request):
+    """
+    Caches checkout data from checkout.
+    From CI Boutique Ado walkthrough.
+    """
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -56,7 +60,11 @@ def checkout(request):
         order_form = OrderForm(form_data)
 
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
