775f0d509a0813d41cb4521a73a3455f0b4bd6e7
JackDay94 <Jack94day@gmail.com>
2023-03-29 12:04:49 +0000

    feat: Add views and templates for blog post list and details

--- a_to_pc/urls.py
+++ a_to_pc/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('wishlist/', include('wishlist.urls')),
     path('newsletter/', include('newsletter.urls')),
+    path('blog/', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import BlogPost, BlogComment
+from .forms import BlogPostForm, BlogCommentForm
+
+
+class PostList(ListView):
+    """Displays a list of blog posts"""
+    model = BlogPost
+    queryset = BlogPost.objects.filter(status=1).order_by('-date_added')
+    template_name = 'blog/blog.html'
+    context_object_name = 'blog_posts'
+    paginate_by = 8
+
+
+class PostDetail(DetailView):
+    """Displays the detail view of a blog post"""
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = BlogPost.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by('-created_on')
+
+        return render(
+            request,
+            "blog/blog_detail.html",
+            {
+                "blog_post": post,
+                "comments": comments,
+                "comment_form": BlogCommentForm(),
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = BlogPost.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by('-created_on')
+        
+        comment_form = BlogCommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.author = request.user
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            messages.success(request, 'Comment added successfully!')
+        else:
+            comment_form = CommentForm()
+            messages.error(request, 'Error! Comment was not added.')
+
+        return render(
+            request,
+            "blog/blog_detail.html",
+            {
+                "blog_post": post,
+                "comments": comments,
+                "comment_form": CommentForm()
+            },
+        )
